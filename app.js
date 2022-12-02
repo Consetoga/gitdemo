1b0d8e0e7eadc5a9cc64d59f9375cb10df716ef3
Conestoga <exceldeveloper3@gmail.com>
2022-12-02 13:52:29 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express")
+
+const app = express()
+
+
+app.get("/",(req,res)=>{
+
+    res.send("Welcome to a Node Js APP!!!")
+})
+
+app.listen(3600,()=>console.log("App is listening at port 3600!!!"))
